b6508ee576fb93a6f3cfb96e7916e8d34ce3f6ab
codeWin <kevin.pirner@live.at>
2016-04-21 20:46:42 +0200

    finalized clients

--- js/custom.js
+++ js/custom.js
@@ -136,6 +136,7 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    
     responsive:{
         0:{
             items:1,
@@ -143,12 +144,12 @@ $('.owl-carousel').owlCarousel({
         },
         600:{
             items:3,
-            nav:false
+            nav:true
         },
         1000:{
             items:5,
             nav:true,
-            loop:false
+            loop:true
     }
   }
 });
