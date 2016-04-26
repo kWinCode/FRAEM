37dbd5159de59d61a2051b856618f89b53dfadba
codeWin <kevin.pirner@live.at>
2016-04-26 13:29:59 +0200

    added preloader

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,4 @@
-/* ==============================================
-	Page Preloader
-=============================================== */
-$(window).load(function() { 
-    $("#loader").delay(500).fadeOut(); 
-    $(".mask").delay(1000).fadeOut("slow");
-});
 
-
-
-
-/* Start Ready Function */
-jQuery(document).ready(function ($) { 
-
-'use strict';
 
 
 /* ==============================================
@@ -161,11 +147,6 @@ $(function(){
     $(".player").mb_YTPlayer();
   });
 
-});
-/* end ready function */
-
-
-    
 
 
 
